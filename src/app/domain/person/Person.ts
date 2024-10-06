30309ee7954f4b3d1404c9f4e7df1783a66bd22c
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-10-06 13:01:44 +0300

    updated

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,13 +1,18 @@
-import { JsonController, Get, Post, Body, Param } from "routing-controllers";
+import { JsonController, Get, Post, Body, Param, UseAfter } from "routing-controllers";
 import { IPerson } from "./Person.types";
 import { ApiResponse } from "helpers/ApiResponse";
 import { ApiError } from "helpers/ApiError";
+import { validate } from 'class-validator';
+import { CreatePerson } from "./CreatePerson.dto";
+import { HTTPResponseLogger } from "app/middlewares/HTTPResponseLogger";
+
 
 const storeData: IPerson[] = [];
 
 @JsonController('/person')
 export default class Person {
     @Get()
+    @UseAfter(HTTPResponseLogger)
     async getAll() {
         return new ApiResponse(true, storeData);
     }
@@ -20,15 +25,27 @@ export default class Person {
         if (!person) {
             throw new ApiError(404, {
                 code: "PERSON_NOT_FOUND",
-                message: `Person woth id ${id} not found`,
+                message: `Person with id ${id} not found`,
             });
         }
         return new ApiResponse(true, person);
     }
 
     @Post()
-    async setPerson(@Body() body: IPerson) {
-        storeData.push(body);
+    async setPerson(@Body() body: CreatePerson) {
+        const errors = await validate(body);
+        if (errors.length > 0) {
+            throw new ApiError(400, {
+                message: "Validation failed",
+                code: "PERSON_VALIDATION_ERROR",
+                errors,
+            });
+        }
+
+        const id = storeData.length;
+
+        storeData.push({...body, id});
+
         return new ApiResponse(true, "Created successfully!");
     }
 }
